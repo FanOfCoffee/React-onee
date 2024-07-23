69bb800e954e504d456ab75ae272dc684e74caeb
FanOfCoffee <keikoshatko@gmail.com>
2024-07-23 21:58:44 +0300

    first-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Работа", 6],
+  ["Дорога на работу", 3],
+  ["Учеба", 3],
+  ["Еда", 2],
+  ["Общение", 1],
+  ["Спорт", 2],
+  ["Сон", 7],
+];
+
+export const options = {
+  title: "Моя активность",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
     </div>
   );
 }
